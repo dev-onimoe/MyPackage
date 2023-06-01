2812886b45e01153e10cb4c9cbda981da3af35b5
Mas'ud <masud@youverify.co>
2023-06-01 09:48:14 +0100

    liveness check

--- Sources/Views/YVOSViewController.swift
+++ Sources/Views/YVOSViewController.swift
@@ -10,7 +10,7 @@ import SafariServices
 
 public class YVOSViewController: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
